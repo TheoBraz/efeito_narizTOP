cffda87eb834df282bf6a031037dff5aa64571f1
TheoBraz <135382477+TheoBraz@users.noreply.github.com>
2023-09-26 16:38:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 coordinadesX = 0;
 coordinadesY = 0;
+coordinadesX2 = 0;
+coordinadesY2 = 0;
 
 function preload() {
   narizVerde = loadImage("Nariz-removebg-preview.png");
+  indoAli = loadImage("indo_ali-removebg.png");
 }
 
 function setup() {
@@ -29,12 +32,17 @@ function gotPoses(results)
     console.log("nose y = " + results[0].pose.nose.y);
     coordinadesX = results[0].pose.nose.x;
     coordinadesY = results[0].pose.nose.y;
+    console.log("rightShoulder x = " + results[0].pose.rightShoulder.x);
+    console.log("rightShoulder y = " + results[0].pose.rightShoulder.y);
+    coordinadesX2 = results[0].pose.rightShoulder.x;
+    coordinadesY2 = results[0].pose.rightShoulder.y;
   }
 }
 
 function draw() {
   image(video, 0, 0, 300, 300);
   image(narizVerde, coordinadesX - 30, coordinadesY - 30, 60, 60);
+  image(indoAli, coordinadesX2 - 15, coordinadesY2 - 80, 60, 60);
 }
 
 function take_snapshot(){    
